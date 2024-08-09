9f0dfb970f36a8658fa414eee82fa2c7fbaf6807
dat <datthao27042002@gmail.com>
2024-08-09 17:05:56 +0700

    fix totalDocument

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -15,7 +15,7 @@ module.exports = {
         status: 200,
         message: "Succes get Users",
         data: users,
-        total: Math.ceil(totalItems / limit),
+        total: users.length(),
       };
     } catch (error) {
       throw error;
